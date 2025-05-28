d5d22abda22687284c3c46a11a91cc4fbe1244a7
Fadi Asbih <fadi@asbih.com>
2025-05-28 10:25:02 +0300

    fix: path for dev env

--- update-skin.sh
+++ update-skin.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
+
+set -e
+
 echo "--------------------"
 echo "Updating version-id in template.xml"
-#get current date and time
 now=$(date '+%Y%m%d-%H%M%S')
 echo "$now"
+
 templatePath="tpl.xml"
-cp $templatePath template.xml
+cp "$templatePath" template.xml
 id='Id'
 sed -i -e "s/$id/$now/g" template.xml
 echo "Version-id in template.xml is set to $now"
 echo "--------------------"
-echo "Compile sass files:"
-echo "Remove the old css-File LUH-Style-1.css..."
-sudo rm LUH-Style/LUH-Style.css
-echo "Compile..."
-sudo sass LUH-Style/scss/LUH-Style.scss LUH-Style/LUH-Style.css
-#sudo chown www-data:www-data LUH-Style.css
-#sudo chown www-data:www-data template.xml
+
+echo "Compiling SASS files for ILIAS skin..."
+
+# Remove the old CSS file
+echo "Removing old LUH-Style.css..."
+rm -f LUH-Style/LUH-Style.css
+
+# Set base path depending on argument
+if [[ "$1" == "dev" ]]; then
+    echo "⚙️  Using development base path: /ilias-luh"
+    base_path="/ilias-luh"
+else
+    echo "⚙️  Using production base path: /"
+    base_path="/"
+fi
+
+# Replace the $base-path value dynamically
+echo "Injecting base path..."
+sed "s|\$base-path: \"/\";|\$base-path: \"$base_path\";|" \
+    LUH-Style/scss/LUH-Style.scss > LUH-Style/scss/LUH-Style.compiled.scss
+
+# Compile with SASS
+echo "Compiling with sass..."
+sass LUH-Style/scss/LUH-Style.compiled.scss LUH-Style/LUH-Style.css
+
+# Cleanup
+rm LUH-Style/scss/LUH-Style.compiled.scss
+
 echo "...done!"
 echo "--------------------"
